803673a26703d786871dcdc6420173e3b16bc1b6
adhitya-arya <106930133+adhitya-arya@users.noreply.github.com>
2022-09-22 00:26:26 +0700

    Update navbar.php

--- user/navbar.php
+++ user/navbar.php
@@ -6,7 +6,7 @@ $gambarku = query("SELECT * FROM users WHERE username = '$username'")[0];
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="download.png" alt="kereta api" width="70px"></a>
+        <a class="navbar-brand" href="index.php"><img src="download.jpg" alt="kereta api" width="70px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
