73e9d2627bf780b1ff180697df2b528b119f25ec
adhitya-arya <106930133+adhitya-arya@users.noreply.github.com>
2022-09-22 00:27:00 +0700

    Update navbar.php

--- admin/navbar.php
+++ admin/navbar.php
@@ -32,7 +32,7 @@
 </style>
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-2 mb-5 bg-body rounded">
     <div class="container-fluid">
-        <a class="navbar-brand" href="admin.php"><img src="download.png" alt="kereta api" width="70px"></a>
+        <a class="navbar-brand" href="admin.php"><img src="download.jpg" alt="kereta api" width="70px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
